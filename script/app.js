6a6d8678148d2736aba31c2a017eec3e3f560e18
camario25 <camario25@yahoo.com>
2018-02-08 16:25:41 -0800

    header animation complete

--- script/app.js
+++ script/app.js
@@ -1,22 +1,51 @@
-const number=100;
+
 $(document).ready(function(){
-    var header=$("header");
-  for(var i=0;i<number;i++){
-    var left=`${Math.floor(Math.random()*100)}vw`;
+  //width of the screen
+  // make i tag load off left of screen
+  // 
+  
+  
+  
+  
+  // function cloudGenerator(){
+  //   var left=`${Math.floor(Math.random()*100)}vw`;
+  //   var top=`${Math.floor(Math.random()*100)+15}vh`;
+  //   var div=$('<div></div>');
+  //   div.addClass('cloud');
+  //   var el=$('<i></i>');
+  //   el.addClass('fas fa-cloud');
+  //   div.append(el);
+  //   div.css({'left':left,'top':top});
+  //   header.append(div);
+  //   console.log(div.position());
+  //   div.animate({left: '-=200'}, 50000,"linear", cloudGenerator());
+  // }
+  
+  function cloudGenerator(){
+    // var el=$('<i></i>');
+    // el.addClass('fas fa-cloud');
+    // 
+    // var div=$('<div></div>');
+    // div.addClass('cloud');
+    var cloud = $("<div class='cloud'><i class='fas fa-cloud'></i></div>");
+    var left= document.body.offsetWidth + cloud.width();
     var top=`${Math.floor(Math.random()*100)+15}vh`;
-  var div=$('<div></div>');
-  div.addClass('cloud');
-  var el=$('<i></i>');
-  el.addClass('fas fa-cloud');
-  div.append(el);
-  div.css({'left':left,'top':top});
-  header.append(div);
-  console.log(div.position());
-}
-$(document).ready(function(){
-  function moveClouds() {
-    $(".fa-cloud").animate({left: -=100}, 5000);
-    };
-  };
+    
+    // div.append(el);
+    cloud.css({'left':left,'top':top});
+    header.append(cloud);
+    console.log(cloud.position());
+    cloud.animate({left: -( cloud.width() ) }, 50000,"linear",function(){
+      cloud.remove();
+    });
+  }
+  
+  var header= $("header");
+  
+//   for(var i=0;i<number;i++){
+// 
+// };
+  setInterval(cloudGenerator,1000);
+  
 });
 
