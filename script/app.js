94927e3c360d30bbb5f236bc0e6441104be305ca
camario25 <camario25@yahoo.com>
2018-02-11 20:14:39 -0800

    added nav links

--- script/app.js
+++ script/app.js
@@ -65,6 +65,20 @@ $(document).ready(function(){
     
 });
 
+$('.nav-links a, .logo a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body').animate({
+    scrollTop: targetOffset
+  }, 600);
+});
 
 $(window).on('scroll', function(){
  if($(window).scrollTop()>780){
